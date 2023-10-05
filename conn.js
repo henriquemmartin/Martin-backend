826d2fadd2cc77cdb3434499eaa909da33cbb5a0
Henrique Martin <henriquemmartin@gmail.com>
2023-10-04 22:22:16 -0300

    alterado URI

--- conn.js
+++ conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function main() {
   const uri =
-    "mongodb+srv://henriquemmartin:Qwerrewq2@cluster0.3jhsdj3.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb://mongo:dMZG90djHw1xnVjyEfzp@containers-us-west-119.railway.app:5736";
   await mongoose.connect(uri);
   console.log("conectado ao bando de dados Primario");
 }
