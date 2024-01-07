5ed0bd6da96b1e92f93cf0c24696f147f4734f6a
Henrique Martin <henriquemmartin@gmail.com>
2024-01-07 10:04:10 -0300

    ok

--- dados.js
+++ dados.js
@@ -66,7 +66,7 @@ app.delete("/message", (req, res) => {
 var contagemLidos = [];
 async function ler2() {
   console.log("função ler2()");
-  contagemLidos = await Contagem.find().sort({ _id: -1 }).limit(500);
+  contagemLidos = await Contagem.find().sort({ _id: -1 }).limit(200);
   console.log("contagem dados lidos com sucesso!");
 }
 
