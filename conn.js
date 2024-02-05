6d6d5c51f8f71b8fa88325b52b54965f02ba5842
Henrique Martin <henriquemmartin@gmail.com>
2024-02-05 20:55:26 -0300

    newurls

--- conn.js
+++ conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function main() {
   const uri =
-    "mongodb://mongo:dMZG90djHw1xnVjyEfzp@containers-us-west-119.railway.app:5736/";
+    "mongodb://mongo:Cf6H16GGgDA-HeF-dha2AeBcEG5EdHG4@roundhouse.proxy.rlwy.net:57345";
   //"mongodb://mongo:dMZG90djHw1xnVjyEfzp@containers-us-west-119.railway.app:5736";
 
   await mongoose.connect(uri);
